07a96689623910fd6586d562522cc776d8cc558a
hailDev <croziputra@gmail.com>
2024-03-29 13:09:02 +0700

    nambah dikit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>tes</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, quidem voluptate hic corrupti dicta deserunt nobis tempora repellat, et quos corporis veritatis odit odio iusto aperiam nihil quasi perspiciatis ad.</p>
   );
 }
    
